600759d351b567a60a9d431e24714b199a7a38f9
HanuYat <jnjn1235@gmail.com>
2026-04-06 10:34:09 +0900

    행렬 파일 임포트

--- Assets/GameMathCurriculum/Ch02/Scripts/Assignment_LissajousCurve.cs
+++ Assets/GameMathCurriculum/Ch02/Scripts/Assignment_LissajousCurve.cs
@@ -71,7 +71,10 @@ public class Assignment_LissajousCurve : MonoBehaviour
         // initialPosition을 기준으로 X, Z 오프셋을 더해 최종 위치를 반환하세요.
 
         // TODO: 위 공식을 구현하세요
-        return initialPosition;
+        float phaseX = amplitudeX * Mathf.Sin(2f * Mathf.PI * frequencyX * time + (this.phaseX * Mathf.Deg2Rad));
+        float phaseZ = amplitudeZ * Mathf.Sin(2f * Mathf.PI * frequencyZ * time + (this.phaseZ * Mathf.Deg2Rad));
+
+        return initialPosition + new Vector3(phaseX, 0f, phaseZ);
     }
 
     private void UpdateDebugUI()
